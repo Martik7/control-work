cd0145a2d14486d9d58ed2634a1f7dde2941a4b0
СадвокасовО <@205_4>
2018-11-27 20:22:33 +0600

    Добавьте файлы проекта.

---
+++ Project7/Source.cpp
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <iostream>
+#include <locale.h>
+
+int main()
+{
+
+	setlocale(LC_ALL, "");
+
+	
+
+	/*int k, H, M;
+
+	k = 3600 / 60;
+
+	printf("¬ведите секунды: ", k);
+	scanf_s("%d", &k);
+
+	H = k / 3600;
+	M = (k % 3600) / 60;
+
+	printf("Ёто %d „асов %d минут\n", H, M);*/
+
+
+
+
+	/*int price = 1000, discount = 5;
+
+	if (price > 400 && price < 600)
+	{
+		printf("скидка 5%");
+	}
+	else if (price < 600 && price < 1000)
+	{
+		printf("скидка 10%");
+	}*/
+
+
+
+	/*int Name, Creator, Size, Path;
+
+	Name = 0;
+	printf("Name - PP-12-1\n", Name);
+
+	Creator = 0;
+	printf("Creator - student\n", Creator);
+
+	Size = 0;
+	printf("Size - 50MB\n", Size);
+
+	Path = 0;
+	printf("Path - D:\Academy\PP-12-1\n", Path);*/
+
+	/*int Name, Avtor, Izdatelstvo, Pages;
+
+	Name = 0;
+
+
+	printf("Name: The war and the peace\n", Name);
+
+	Avtor = 0;
+
+	printf("Avtor: L.N. Tolstoj\n", Avtor);
+
+	Izdatelstvo = 0;
+
+	printf("Izdatelstvo: Piter\n", Izdatelstvo);
+
+	Pages = 0;
+
+	printf("Pages: 500\n", Pages);*/
+
+
+	system("pause");
+
+}
